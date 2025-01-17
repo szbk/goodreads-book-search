7a89f1bd70ef9c2b66a0dfa9cf884186d72baccc
szbk <wisecolt@gmail.com>
2025-01-17 18:56:16 +0300

    Test format update

--- test/index.js
+++ test/index.js
@@ -2,19 +2,28 @@ const { expect } = require("chai");
 
 const GoodreadsBookSearch = require("../index");
 
-describe("🧠 Goodreads Book Search Integration Test:", () => {
-
+describe("🧠 Goodreads Book Search Integration Test", () => {
     let bookSearch;
     const isbn = "9944824453";
-    const timeoutDuration = 10000;
+    const timeoutDuration = 30000;
 
     beforeEach(() => {
         bookSearch = new GoodreadsBookSearch();
     });
 
-    it('Is the book title "Dövmeli Adam"? 🚀', function (done) {
+    it('Is the ISBN "9944824453"? 🔥', function (done) {
+        this.timeout(timeoutDuration);
+        bookSearch.getBookDetails(isbn)
+            .then((bookDetails) => {
+                expect(bookDetails).to.have.property("isbn");
+                expect(bookDetails.isbn).to.equal("9944824453");
+                done();
+            })
+            .catch(done);
+    });
 
-        this.timeout(timeoutDuration)
+    it('Is the book title "Dövmeli Adam"? 🚀', function (done) {
+        this.timeout(timeoutDuration);
         bookSearch.getBookDetails(isbn)
             .then((bookDetails) => {
                 expect(bookDetails).to.have.property("title");
@@ -24,33 +33,8 @@ describe("🧠 Goodreads Book Search Integration Test:", () => {
             .catch(done);
     });
 
-    it('Is the author\'s name "Peter V. Brett"? 💡', function (done) {
-
-        this.timeout(timeoutDuration);
-        bookSearch.getBookDetails(isbn)
-            .then((bookDetails) => {
-                expect(bookDetails).to.have.property("authorName");
-                expect(bookDetails.authorName.author.name).to.equal("Peter V. Brett");
-                done();
-            })
-            .catch(done);
-    });
-
-    it('Is the page count "640"? 📋', function (done) {
-
-        this.timeout(timeoutDuration);
-        bookSearch.getBookDetails(isbn)
-            .then((bookDetails) => {
-                expect(bookDetails).to.have.property("page");
-                expect(bookDetails.page).to.equal("640");
-                done();
-            })
-            .catch(done);
-    });
-
     it('Is the book\'s publication date "1 September 2008"? ⏰', function (done) {
         this.timeout(timeoutDuration);
-
         bookSearch.getBookDetails(isbn)
             .then((bookDetails) => {
                 expect(bookDetails).to.have.property("date");
@@ -60,13 +44,12 @@ describe("🧠 Goodreads Book Search Integration Test:", () => {
             .catch(done);
     });
 
-    it('Is the ISBN "9944824453"? 🔥', function (done) {
-
+    it('Is the page count "640"? 📋', function (done) {
         this.timeout(timeoutDuration);
         bookSearch.getBookDetails(isbn)
             .then((bookDetails) => {
-                expect(bookDetails).to.have.property("isbn");
-                expect(bookDetails.isbn).to.equal("9944824453");
+                expect(bookDetails).to.have.property("page");
+                expect(bookDetails.page).to.equal("640");
                 done();
             })
             .catch(done);
